a469fd963b5d8cd3641e53d98df2047b0b355918
sot26 <omotoshotobi26@gmail.com>
2024-10-12 17:33:59 +0100

    collaboration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
